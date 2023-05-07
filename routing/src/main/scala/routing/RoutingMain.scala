ecdefc54728d094d3303e9d75a82dc978f01a149
Nikita2305 <44189711+Nikita2305@users.noreply.github.com>
2023-05-07 15:42:14 +0300

    Контейнеризация
    HW3:
    * sbt-assembly
    * Dockerfiles + docker-compose
    * mock API

--- routing/src/main/scala/routing/RoutingMain.scala
+++ routing/src/main/scala/routing/RoutingMain.scala
@@ -9,8 +9,9 @@ object RoutingMain extends ZIOAppDefault {
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     for {
-      _ <- ZIO.logInfo("Starting TemplateMain")
-      _ <- Server.serve(RoutingRoutes.app)
+      _ <- ZIO.logInfo("Starting Routing service")
+      _ <- Server
+        .serve(RoutingRoutes.app)
         .provide(
           Server.live,
           ServiceConfig.live
--- routing/src/main/scala/routing/api/RoutingRoutes.scala
+++ routing/src/main/scala/routing/api/RoutingRoutes.scala
@@ -1,9 +1,30 @@
 package routing.api
 
+import zio.ZIO
 import zio.http._
+import zio.http.model.{Method, Status}
 
 object RoutingRoutes {
 
-  val app: Http[Any, Response, Request, Response] = ???
+  val app: Http[Any, Response, Request, Response] = Http.collectZIO[Request] {
+
+    case Method.GET -> !! / "hello" =>
+      ZIO.succeed(Response.text("Hello world!"))
+
+    case request @ Method.POST -> !! / "echo" =>
+      val response =
+        for {
+          input <- ZIO
+            .fromOption(
+              request.url.queryParams
+                .get("input")
+                .flatMap(_.headOption)
+            )
+            .tapError(_ => ZIO.logError("bad request"))
+        } yield Response.text(input)
+
+      response.orElseFail(Response.status(Status.BadRequest))
+
+  }
 
 }
