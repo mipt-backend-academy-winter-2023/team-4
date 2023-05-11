be04a66ddd4fc18e5b69c80724b7938c6a475ce6
d.podpryatov <d.podpryatov@tinkoff.ru>
2023-05-11 23:31:22 +0300

    Setup homework 4

--- project/Libs.scala
+++ project/Libs.scala
@@ -5,17 +5,33 @@ object Libs {
   private object V {
     val zio = "2.0.13"
     val zioHttp = "0.0.5"
+    val zioPostgres = "0.1.2"
 
     val pureconfig = "0.17.3"
+
+    val flyway = "9.17.0"
+
+    val circe = "0.14.5"
   }
 
   val zio: Seq[ModuleID] = Seq(
     "dev.zio" %% "zio" % V.zio,
-    "dev.zio" %% "zio-http" % V.zioHttp
+    "dev.zio" %% "zio-http" % V.zioHttp,
+    "dev.zio" %% "zio-sql-postgres" % V.zioPostgres
   )
 
   val pureconfig: Seq[ModuleID] = Seq(
     "com.github.pureconfig" %% "pureconfig" % V.pureconfig
+  )
+
+  val flyway: Seq[ModuleID] = Seq(
+    "org.flywaydb" % "flyway-core" % V.flyway
+  )
+
+  val circe: Seq[ModuleID] = Seq(
+    "io.circe" %% "circe-core" % V.circe,
+    "io.circe" %% "circe-generic" % V.circe,
+    "io.circe" %% "circe-parser" % V.circe
   )
 
 }
