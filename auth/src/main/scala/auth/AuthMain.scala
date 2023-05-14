05e1ce81d581b88414f9c0154bb8dc9be0d0ab3c
Alekseev Maksim <ma.i.alekseev@tinkoff.ru>
2023-05-14 23:50:41 +0300

    first version

--- auth/src/main/scala/auth/AuthMain.scala
+++ auth/src/main/scala/auth/AuthMain.scala
@@ -4,17 +4,26 @@ import auth.api.AuthRoutes
 import auth.config.ServiceConfig
 import zio.http.Server
 import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
+import config.Config
+import flyway.FlywayAdapter
 
 object AuthMain extends ZIOAppDefault {
 
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
-    for {
-      _ <- ZIO.logInfo("Starting Auth service")
-      _ <- Server.serve(AuthRoutes.app)
-        .provide(
-          Server.live,
-          ServiceConfig.live
-        )
-    } yield ()
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
+    val server =
+      for {
+        flyway <- ZIO.service[FlywayAdapter.Service]
+        _ <- flyway.migration
+        server <- zio.http.Server.serve(AuthRoutes.app)
+      } yield ()
+//    for {
+//      _ <- ZIO.logInfo("Starting Auth service")
+//      _ <- Server.serve(AuthRoutes.app)
+//        .provide(
+//          Server.live,
+//          ServiceConfig.live
+//        )
+//    } yield ()
+  }
 
 }
