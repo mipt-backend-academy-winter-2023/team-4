d7c19446d221d767ee4dac624c9e80c329d43715
Annovid <nil.nil532@gmail.com>
2023-08-30 00:45:45 +0300

    add hw2

--- auth/src/main/scala/auth/api/AuthRoutes.scala
+++ auth/src/main/scala/auth/api/AuthRoutes.scala
@@ -1,9 +1,57 @@
 package auth.api
 
+import zio.ZIO
 import zio.http._
+import zio.http.model.{Method, Status}
 
 object AuthRoutes {
-
-  val app: Http[Any, Response, Request, Response] = ???
-
+  val app: Http[Any, Response, Request, Response] = Http.collectZIO[Request] {
+    //  Method GET for testing
+    case Method.GET -> !! =>
+      ZIO.succeed(Response.text("The Auth service is working"))
+    case request@Method.POST -> !! / "auth" / "register" =>
+      val response = for {
+        username <- ZIO.fromOption(
+          request
+            .url
+            .queryParams
+            .get("username")
+            .flatMap(_.headOption)
+        ).tapError(
+          _ => ZIO.logError("There is not field username in the request body")
+        )
+        password <- ZIO.fromOption(
+          request
+            .url
+            .queryParams
+            .get("password")
+            .flatMap(_.headOption)
+        ).tapError(
+          _ => ZIO.logError("There is not field password in the request body")
+        )
+      } yield Response.text(s"Successful user registration $username with password $password")
+      response.orElseFail(Response.status(Status.BadRequest))
+    case request@Method.POST -> !! / "auth" / "login" =>
+      val response = for {
+        username <- ZIO.fromOption(
+          request
+            .url
+            .queryParams
+            .get("username")
+            .flatMap(_.headOption)
+        ).tapError(
+          _ => ZIO.logError("There is not field username in the request body")
+        )
+        password <- ZIO.fromOption(
+          request
+            .url
+            .queryParams
+            .get("password")
+            .flatMap(_.headOption)
+        ).tapError(
+          _ => ZIO.logError("There is not field password in the request body")
+        )
+      } yield Response.text(s"Successful authorisation for user $username with password $password")
+      response.orElseFail(Response.status(Status.BadRequest))
+  }
 }
--- routing/src/main/scala/routing/api/RoutingRoutes.scala
+++ routing/src/main/scala/routing/api/RoutingRoutes.scala
@@ -1,9 +1,27 @@
 package routing.api
 
+import zio.ZIO
 import zio.http._
+import zio.http.model.{Method, Status}
 
 object RoutingRoutes {
 
-  val app: Http[Any, Response, Request, Response] = ???
-
+  val app: Http[Any, Response, Request, Response] = Http.collectZIO[Request] {
+    //  Method GET for testing
+    case Method.GET -> !! =>
+      ZIO.succeed(Response.text("The Routing service is working"))
+    case request@Method.POST -> !! / "auth" / "components" / "schemas" / "points" =>
+    val response = for {
+      point_ids <- ZIO.fromOption(
+          request
+            .url
+            .queryParams
+            .get("points")
+            .flatMap(_.headOption)
+        ).tapError(
+          _ => ZIO.logError("There is not field points in the request body")
+        )
+      } yield Response.text(s"Routing request for points $point_ids")
+      response.orElseFail(Response.status(Status.BadRequest))
+  }
 }
