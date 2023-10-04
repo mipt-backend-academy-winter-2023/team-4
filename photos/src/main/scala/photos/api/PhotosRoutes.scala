ec350511c7c0235f83450f39cb1fc7df0bc5e554
Maksim Alekseev <maksimalekseev@Maksims-MacBook-Pro.local>
2023-10-05 00:08:36 +0300

    done

--- photos/src/main/scala/photos/api/PhotosRoutes.scala
+++ photos/src/main/scala/photos/api/PhotosRoutes.scala
@@ -5,13 +5,33 @@ import zio.ZIO
 import zio.http._
 import zio.http.model.Method
 import zio.http.model.Status
-import zio.stream.{ZSink, ZStream}
+import zio.stream.{ZSink, ZStream, ZPipeline}
+import java.nio.file.{Files, Paths}
 
 object PhotosRoutes {
+
+    def getPath(id: String) = Paths.get(s"/${id}_image.png")
+
     val app: HttpApp[Any, Response] = 
-        Http.CollectZIO[Request] {
-            case req@Method.POST -> !! / "photo" / id => {
+        Http.collectZIO[Request] {
+            case req@Method.PUT -> !! / "photo" / id => 
+                (for {
+                    path <- ZIO.attempt(Files.createFile(getPath(id)))
+                    size <- req.body.asStream.via(ZPipeline.deflate()).run(ZSink.fromPath(path))
+                } yield size).either.map {
+                    case Left(_) => Response.status(Status.BadRequest)
+                    case Right(_) => Response.status(Status.Ok)
+                }
+            
+            case req@Method.GET -> !! / "photo" / id =>
+                if (Files.exists(getPath(id))) {
+                    ZIO.succeed(Response(
+                        status = Status.Ok,
+                        body = Body.fromStream(ZStream.fromFile(getPath(id).toFile))
+                    ))
+                } else {
+                    ZIO.succeed(Response.status(Status.NotFound))
+                }
                 
-            }
         }
 }
