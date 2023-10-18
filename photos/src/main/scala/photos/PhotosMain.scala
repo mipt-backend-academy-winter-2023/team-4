01bc0e3be34e6581dfe1e7a88f450ac7538f35f4
Nikita Nikitin <nikitanikitin2305@gmail.com>
2023-10-18 23:56:37 +0300

    feat: add CI

--- photos/src/main/scala/photos/PhotosMain.scala
+++ photos/src/main/scala/photos/PhotosMain.scala
@@ -7,14 +7,14 @@ import zio.{ZIO, ZIOAppDefault, ZIOAppArgs, Scope}
 
 object PhotosMain extends ZIOAppDefault {
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
-     val server =
-        for {
-          _ <- ZIO.logInfo("Starting Photos service")
-          server <- zio.http.Server.serve(PhotosRoutes.app)
-        } yield ()
-     server.provide(
-       Server.live,
-       ServiceConfig.live
-     )
+    val server =
+      for {
+        _ <- ZIO.logInfo("Starting Photos service")
+        server <- zio.http.Server.serve(PhotosRoutes.app)
+      } yield ()
+    server.provide(
+      Server.live,
+      ServiceConfig.live
+    )
   }
 }
